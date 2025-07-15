1fa37366972188c65085bc7487c0879da228b46b
Kartikleo <kartiksangwan2004@gmail.com>
2025-07-15 12:39:41 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   dialect: "postgresql", // "mysql" | "sqlite" | "postgresql"
   schema: "./utils/schema.js",
   dbCredentials:{
-    url:'postgresql://neondb_owner:npg_m0tMxWh2frDZ@ep-damp-bar-a8ecaq32-pooler.eastus2.azure.neon.tech/neondb?sslmode=require'
+    url:'YOUR_API_POSTGRE_SQL'
   }
 };
